bc0744098c7e71de95ad19d765ac5d020dd86ab5
Paulo Weber <paulloweber@gmail.com>
2023-01-17 01:26:40 -0300

    add new routes

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -16,5 +16,17 @@ export class UserController {
   @ApiOperation({ summary: 'Create User' })
   async create(@Body() createUserDto: CreateUserDto) {
     return await this.userService.create(createUserDto);
+  }
+
+  @Get('cpf/:cpf')
+  @ApiOperation({ summary: 'Find User By CPF' })
+  async findByCpf(@Param('cpf') cpf: string) {
+    return await this.userService.findByCPF(cpf);
+  }
+
+  @Get('email/:email')
+  @ApiOperation({ summary: 'Find User By Email' })
+  async findByEmail(@Param('email') cpf: string) {
+    return await this.userService.findByEmail(cpf);
   }
 }
